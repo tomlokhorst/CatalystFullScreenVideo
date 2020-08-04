a987a2dc035e74de8937ef52de7db4462249fe96
Tom Lokhorst <tom@lokhorst.eu>
2020-08-04 16:41:52 +0200

    Allow background playback / Pip

--- CatalystFullScreenVideo/AppDelegate.swift
+++ CatalystFullScreenVideo/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        // Configure the shared AV session
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.playback)
+            try audioSession.setMode(.moviePlayback)
+        } catch {
+            assertionFailure("Configuring AVAudioSession failed.")
+        }
+
+
         return true
     }
 
